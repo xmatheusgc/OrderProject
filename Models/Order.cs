cd7cc8d7ebbb2079f61ada67e32299129051963e
Matheus Garcia <matheusgc1006@gmail.com>
2026-05-11 09:20:59 -0300

    feat: add Order entity model

---
+++ Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderApi.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string CustomerName { get; set; } = string.Empty;
+
+        public decimal Value { get; set; }
+
+        public DateTime OrderDate { get; set; }
+    }
+}
