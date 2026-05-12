962a6640ee85993dc1de015307214a0fb5bfb749
Matheus Garcia <matheusgc1006@gmail.com>
2026-05-12 11:12:27 -0300

    feat: configure docker environment

--- Program.cs
+++ Program.cs
@@ -20,6 +20,12 @@ builder.Services.AddScoped<IOrderService, OrderService>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
